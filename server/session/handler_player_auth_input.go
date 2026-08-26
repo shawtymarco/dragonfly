5fe3475cd9c88be8d09e2f8eea8bf885b56c25c8
marcolol <150941290+shawtymarco@users.noreply.github.com>
2026-08-26 18:21:50 +0900

    server/session: mark movement dirty and use fast break config

--- server/session/handler_player_auth_input.go
+++ server/session/handler_player_auth_input.go
@@ -33,28 +33,18 @@ func (h PlayerAuthInputHandler) handleMovement(pk *packet.PlayerAuthInput, s *Se
 	for i, v := range [...]*float32{&pk.Pitch, &pk.Yaw, &pk.HeadYaw, &pk.Position[0], &pk.Position[1], &pk.Position[2]} {
 		f := float64(*v)
 		if math.IsNaN(f) || math.IsInf(f, 1) || math.IsInf(f, 0) {
-			// Sometimes, the PlayerAuthInput packet is in fact sent with NaN/INF after being teleported (to another
-			// world), see #425. For this reason, we don't actually return an error if this happens, because this will
-			// result in the player being kicked. Just log it and replace the NaN value with the one we have tracked
-			// server-side.
 			s.conf.Log.Debug("process packet: PlayerAuthInput: found nan/inf values. assuming server-side values", "pos", fmt.Sprint(pk.Position), "yaw", pk.Yaw, "head-yaw", pk.HeadYaw, "pitch", pk.Pitch)
 			*v = float32(reference[i])
 		}
 	}
 
-	pk.Position = pk.Position.Sub(mgl32.Vec3{0, 1.62}) // Sub the base offset of players from the pos.
-
+	pk.Position = pk.Position.Sub(mgl32.Vec3{0, 1.62})
 	newPos := vec32To64(pk.Position)
 	deltaPos, deltaYaw, deltaPitch := newPos.Sub(pos), float64(pk.Yaw)-yaw, float64(pk.Pitch)-pitch
 
-	// The PlayerAuthInput packet is sent every tick, so don't check for teleport if the position and rotation
-	// were unchanged.
 	if !mgl64.FloatEqual(deltaPos.Len(), 0) || !mgl64.FloatEqual(deltaYaw, 0) || !mgl64.FloatEqual(deltaPitch, 0) {
 		if expected := s.teleportPos.Load(); expected != nil {
 			if newPos.Sub(*expected).Len() > 1 {
-				// The player has moved before it received the teleport packet. Ignore this movement entirely and
-				// wait for the client to sync itself back to the server. Once we get a movement that is close
-				// enough to the teleport position, we'll allow the player to move around again.
 				return nil
 			}
 			s.teleportPos.Store(nil)
@@ -63,6 +53,9 @@ func (h PlayerAuthInputHandler) handleMovement(pk *packet.PlayerAuthInput, s *Se
 
 	s.moving = true
 	c.Move(deltaPos, deltaYaw, deltaPitch)
+	if !deltaPos.ApproxEqual(mgl64.Vec3{}) {
+		world.MarkEntityMovementDirty(c)
+	}
 	return nil
 }
 
@@ -73,7 +66,7 @@ func (h PlayerAuthInputHandler) handleActions(pk *packet.PlayerAuthInput, s *Ses
 		if !ok {
 			return fmt.Errorf("item interaction flag set without item interaction data")
 		}
-		if err := h.handleUseItemData(data, s, c); err != nil {
+		if err := h.handleUseItemData(data, s, tx, c); err != nil {
 			return err
 		}
 	}
@@ -82,7 +75,7 @@ func (h PlayerAuthInputHandler) handleActions(pk *packet.PlayerAuthInput, s *Ses
 		if !ok {
 			return fmt.Errorf("block actions flag set without block actions")
 		}
-		if err := h.handleBlockActions(actions, s, c); err != nil {
+		if err := h.handleBlockActions(actions, s, tx, c); err != nil {
 			return err
 		}
 	}
@@ -96,11 +89,8 @@ func (h PlayerAuthInputHandler) handleActions(pk *packet.PlayerAuthInput, s *Ses
 		s.inTransaction.Store(true)
 		defer s.inTransaction.Store(false)
 
-		// As of 1.18 this is now used for sending item stack requests such as when mining a block.
 		sh := s.handlers[packet.IDItemStackRequest].(*ItemStackRequestHandler)
 		if err := sh.handleRequest(request, s, tx, c); err != nil {
-			// Item stacks being out of sync isn't uncommon, so don't error. Just debug the error and let the
-			// revert do its work.
 			s.conf.Log.Debug("process packet: PlayerAuthInput: resolve item stack request: " + err.Error())
 		}
 	}
@@ -162,7 +152,7 @@ func (h PlayerAuthInputHandler) handleInputFlags(flags protocol.InputFlags, s *S
 }
 
 // handleUseItemData handles the protocol.UseItemTransactionData found in a packet.PlayerAuthInput.
-func (h PlayerAuthInputHandler) handleUseItemData(data protocol.UseItemTransactionData, s *Session, c Controllable) error {
+func (h PlayerAuthInputHandler) handleUseItemData(data protocol.UseItemTransactionData, s *Session, tx *world.Tx, c Controllable) error {
 	s.swingingArm.Store(true)
 	defer s.swingingArm.Store(false)
 
@@ -173,9 +163,14 @@ func (h PlayerAuthInputHandler) handleUseItemData(data protocol.UseItemTransacti
 	}
 	pos := cube.Pos{int(data.BlockPosition[0]), int(data.BlockPosition[1]), int(data.BlockPosition[2])}
 
-	// Seems like this is only used for breaking blocks at the moment.
 	switch data.ActionType {
 	case protocol.UseItemActionBreakBlock:
+		if tx.MinigameConfig().FastBreakBlock {
+			if fast, ok := c.(minigameFastBlockBreaker); ok {
+				fast.FastBreakBlock(pos)
+				return nil
+			}
+		}
 		c.BreakBlock(pos)
 	default:
 		return fmt.Errorf("unhandled UseItem ActionType for PlayerAuthInput packet %v", data.ActionType)
@@ -184,9 +179,9 @@ func (h PlayerAuthInputHandler) handleUseItemData(data protocol.UseItemTransacti
 }
 
 // handleBlockActions handles a slice of protocol.PlayerBlockAction present in a PlayerAuthInput packet.
-func (h PlayerAuthInputHandler) handleBlockActions(a []protocol.PlayerBlockAction, s *Session, c Controllable) error {
+func (h PlayerAuthInputHandler) handleBlockActions(a []protocol.PlayerBlockAction, s *Session, tx *world.Tx, c Controllable) error {
 	for _, action := range a {
-		if err := handlePlayerAction(action.Action, action.Face, action.BlockPos, selfEntityRuntimeID, s, c); err != nil {
+		if err := handlePlayerAction(action.Action, action.Face, action.BlockPos, selfEntityRuntimeID, s, tx, c); err != nil {
 			return err
 		}
 	}
